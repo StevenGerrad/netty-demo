f726cfa81d54fadc9164ad400480dba8a3007971
StevenGerrad <1339807619@qq.com>
2023-09-28 19:36:25 +0800

    P26 nio-非阻塞模式-调试

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # netty-demo
+
+> 课程地址：[黑马程序员Netty全套教程， netty深入浅出Java网络编程教程](https://www.bilibili.com/video/BV1py4y1E7oA)
+
 the project practice for netty class with uper BlackHorse in bilibili
--- src/test/java/cn/itcast/netty/c4/Server.java
+++ src/test/java/cn/itcast/netty/c4/Server.java
@@ -16,12 +16,12 @@ import static cn.itcast.netty.c1.ByteBufferUtil.debugRead;
 @Slf4j
 public class Server {
     public static void main(String[] args) throws IOException {
-        // 使用 nio 来理解阻塞模式，单线程
+        // 使用 nio 来理解非阻塞模式，单线程
         // 0. ByteBuffer
         ByteBuffer buffer = ByteBuffer.allocate(16);
         // 1. 创建了服务器
         ServerSocketChannel ssc = ServerSocketChannel.open();
-
+        ssc.configureBlocking(false);       // 使用后，下面变为非阻塞的
         // 2. 绑定监听端口
         ssc.bind(new InetSocketAddress(8080));
 
@@ -29,18 +29,26 @@ public class Server {
         List<SocketChannel> channels = new ArrayList<>();
         while(true){
             // 4. accept 建立与客户端连接，SocketChannel 用来与客户端通信
-            log.debug("connecting...");
-            SocketChannel sc = ssc.accept();    // 阻塞方法，线程停止运行
-            log.debug("connecting... {}", sc);
-            channels.add(sc);
+            SocketChannel sc = ssc.accept();
+            // 阻塞方法，线程停止运行
+            // 非阻塞时，线程还会继续运行，如果没有连接建立，sc是null
+            if(sc != null){
+                log.debug("connecting... {}", sc);
+                sc.configureBlocking(false);
+                channels.add(sc);
+            }
             for(SocketChannel channel : channels){
                 // 5. 接受客户端发送的数据
-                log.debug("before read... {}", channel);
-                channel.read(buffer);   // 阻塞方法，线程停止运行
-                buffer.flip();
-                debugRead(buffer);
-                buffer.clear();
-                log.debug("after read.. {}", channel);
+                int read = channel.read(buffer);
+                // 阻塞方法，线程停止运行
+                // SocketChannel.configureBlocking 设置非阻塞，如果没有读到数据，read返回0
+                if(read > 0){
+                    buffer.flip();
+                    debugRead(buffer);
+                    buffer.clear();
+                    log.debug("after read.. {}", channel);
+                }
+
             }
         }
     }
