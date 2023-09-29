4f0537bded36d6b99187ec59ae39ca97e2eb1db0
StevenGerrad <1339807619@qq.com>
2023-09-29 21:32:41 +0800

    P46 nio-多线程优化-多worker

---
+++ src/test/java/cn/itcast/netty/c4/MultiThreadServer2.java
@@ -0,0 +1,102 @@
+package cn.itcast.netty.c4;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static cn.itcast.nio.c2.ByteBufferUtil.debugAll;
+
+@Slf4j
+public class MultiThreadServer2 {
+    public static void main(String[] args) throws IOException {
+        // Boss只负责建立连接，Worker复制执行事件
+        Thread.currentThread().setName("boss");
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.configureBlocking(false);
+        Selector boss = Selector.open();
+        SelectionKey bossKey = ssc.register(boss, 0, null);
+        bossKey.interestOps(SelectionKey.OP_ACCEPT);
+        ssc.bind(new InetSocketAddress(8080));
+        // 1. 创建固定数量的 worker 并初始化
+        Worker[] workers = new Worker[Runtime.getRuntime().availableProcessors()];  // 动态获取机器的可用核心数
+        for (int i = 0; i < workers.length; i++) {
+            workers[i] = new Worker("worker-" + i);
+        }
+        AtomicInteger index = new AtomicInteger();
+        while(true) {
+            boss.select();
+            Iterator<SelectionKey> iter = boss.selectedKeys().iterator();
+            while (iter.hasNext()) {
+                SelectionKey key = iter.next();
+                iter.remove();
+                if (key.isAcceptable()) {
+                    SocketChannel sc = ssc.accept();
+                    sc.configureBlocking(false);
+                    log.debug("connected...{}", sc.getRemoteAddress());
+                    // 2. 关联 selector
+                    log.debug("before register...{}", sc.getRemoteAddress());
+                    // round robin 轮询
+                    workers[index.getAndIncrement() % workers.length].register(sc); // boss 调用 初始化 selector , 启动 worker-0
+                    log.debug("after register...{}", sc.getRemoteAddress());
+                }
+            }
+        }
+    }
+    static class Worker implements Runnable{
+        private Thread thread;
+        private Selector selector;
+        private String name;
+        private volatile boolean start = false; // 还未初始化
+        private ConcurrentLinkedQueue<Runnable> queue = new ConcurrentLinkedQueue<>();
+        public Worker(String name) {
+            this.name = name;
+        }
+
+        // 初始化线程，和 selector
+        public void register(SocketChannel sc) throws IOException {
+            // 线程只创建一次
+            if(!start) {
+                selector = Selector.open();
+                thread = new Thread(this, name);
+                thread.start();
+                start = true;
+            }
+            selector.wakeup(); // 唤醒 select 方法 boss
+            sc.register(selector, SelectionKey.OP_READ, null); // boss
+        }
+
+        /*
+        * 注意 Worker的职责，即监控读写事件
+        * */
+        @Override
+        public void run() {
+            while(true) {
+                try {
+                    selector.select(); // worker-0  阻塞
+                    Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
+                    while (iter.hasNext()) {
+                        SelectionKey key = iter.next();
+                        iter.remove();
+                        if (key.isReadable()) {
+                            ByteBuffer buffer = ByteBuffer.allocate(16);
+                            SocketChannel channel = (SocketChannel) key.channel();
+                            log.debug("read...{}", channel.getRemoteAddress());
+                            channel.read(buffer);
+                            buffer.flip();
+                            debugAll(buffer);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
+
