97eee7c999bbe2550dc00e89b738679f1ba266fc
StevenGerrad <1339807619@qq.com>
2023-09-29 11:20:39 +0800

    P37 nio-selector-处理可写事件

--- src/test/java/cn/itcast/netty/c4/WriteServer.java
+++ src/test/java/cn/itcast/netty/c4/WriteServer.java
@@ -23,7 +23,7 @@ public class WriteServer {
                 if (key.isAcceptable()) {
                     SocketChannel sc = ssc.accept();
                     sc.configureBlocking(false);
-                    // SelectionKey sckey = sc.register(selector, 0, null);
+                    SelectionKey sckey = sc.register(selector, 0, null);
                     // sckey.interestOps(SelectionKey.OP_READ);
                     // 1. 向客户端发送大量数据
                     StringBuilder sb = new StringBuilder();
@@ -31,34 +31,36 @@ public class WriteServer {
                         sb.append("a");
                     }
                     ByteBuffer buffer = Charset.defaultCharset().encode(sb.toString());
-                    while (buffer.hasRemaining()) {
-                        int write = sc.write(buffer);
-                        System.out.println(write);
+                    // while (buffer.hasRemaining()) {
+                    //     int write = sc.write(buffer);
+                    //     System.out.println(write);
+                    // }
+                    // 2. 返回值代表实际写入的字节数
+                    int write = sc.write(buffer);   // write不能保证一次全部写入
+                    System.out.println(write);
+
+                    // 3. 判断是否有剩余内容
+                    if (buffer.hasRemaining()) {
+                        // sckey.interestOps(SelectionKey.OP_WRITE);    // 如果这样写，相当于写事件把原来的读事件覆盖了
+                        // 4. 关注可写事件   读事件：1   写事件：4
+                        sckey.interestOps(sckey.interestOps() + SelectionKey.OP_WRITE);
+                        // sckey.interestOps(sckey.interestOps() | SelectionKey.OP_WRITE);
+
+                        // 5. 把未写完的数据挂到 sckey 上
+                        sckey.attach(buffer);
                     }
-//                     // 2. 返回值代表实际写入的字节数
-//                     int write = sc.write(buffer);   // write不能保证一次全部写入
-//                     System.out.println(write);
-//
-//                     // 3. 判断是否有剩余内容
-//                     if (buffer.hasRemaining()) {
-//                         // 4. 关注可写事件   1                     4
-//                         sckey.interestOps(sckey.interestOps() + SelectionKey.OP_WRITE);
-// //                        sckey.interestOps(sckey.interestOps() | SelectionKey.OP_WRITE);
-//                         // 5. 把未写完的数据挂到 sckey 上
-//                         sckey.attach(buffer);
-//                     }
                 }
-                // else if (key.isWritable()) {
-                //     ByteBuffer buffer = (ByteBuffer) key.attachment();
-                //     SocketChannel sc = (SocketChannel) key.channel();
-                //     int write = sc.write(buffer);
-                //     System.out.println(write);
-                //     // 6. 清理操作
-                //     if (!buffer.hasRemaining()) {
-                //         key.attach(null); // 需要清除buffer
-                //         key.interestOps(key.interestOps() - SelectionKey.OP_WRITE);//不需关注可写事件
-                //     }
-                // }
+                else if (key.isWritable()) {
+                    ByteBuffer buffer = (ByteBuffer) key.attachment();
+                    SocketChannel sc = (SocketChannel) key.channel();
+                    int write = sc.write(buffer);
+                    System.out.println(write);
+                    // 6. 清理操作
+                    if (!buffer.hasRemaining()) {
+                        key.attach(null); // 需要清除buffer
+                        key.interestOps(key.interestOps() - SelectionKey.OP_WRITE);//不需关注可写事件
+                    }
+                }
             }
         }
     }
