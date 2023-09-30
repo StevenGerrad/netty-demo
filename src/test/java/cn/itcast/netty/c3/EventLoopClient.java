39c72b31e2d89da47653c839484f2b293dd5b378
StevenGerrad <1339807619@qq.com>
2023-09-30 15:51:15 +0800

    P60 netty入门-eventloop-io任务

---
+++ src/test/java/cn/itcast/netty/c3/EventLoopClient.java
@@ -0,0 +1,68 @@
+package cn.itcast.netty.c3;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.InetSocketAddress;
+
+@Slf4j
+public class EventLoopClient {
+    public static void main(String[] args) throws InterruptedException {
+
+        // 1. 启动类
+        Channel channel = new Bootstrap()
+                // 2. 添加 EventLoop（客户端可以用，也可以不用netty，用之前的nio也可以）
+                .group(new NioEventLoopGroup())
+                // 3. 选择客户端 channel 实现
+                .channel(NioSocketChannel.class)
+                // 4. 添加处理器
+                .handler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override // 在连接建立后被调用
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                })
+                // 5. 连接到服务器
+                .connect(new InetSocketAddress("localhost", 8080))
+                .sync()
+                .channel();
+        // channel.writeAndFlush("hello");
+        System.out.println(channel);
+        System.out.println("");
+
+        // 2. 带有 Future，Promise 的类型都是和异步方法配套使用，用来处理结果
+        // ChannelFuture channelFuture = new Bootstrap()
+        //         .group(new NioEventLoopGroup())
+        //         .channel(NioSocketChannel.class)
+        //         .handler(new ChannelInitializer<NioSocketChannel>() {
+        //             @Override // 在连接建立后被调用
+        //             protected void initChannel(NioSocketChannel ch) throws Exception {
+        //                 ch.pipeline().addLast(new StringEncoder());
+        //             }
+        //         })
+        //         // 1. 连接到服务器
+        //         // 异步非阻塞, main 发起了调用，真正执行 connect 是 nio 线程
+        //         .connect(new InetSocketAddress("localhost", 8080)); // 1s 秒后
+
+        // 2.1 使用 sync 方法同步处理结果
+        // channelFuture.sync(); // 阻塞住当前线程，直到nio线程连接建立完毕
+        // Channel channel = channelFuture.channel();
+        // log.debug("{}", channel);
+        // channel.writeAndFlush("hello, world");
+
+        // 2.2 使用 addListener(回调对象) 方法异步处理结果
+        // channelFuture.addListener(new ChannelFutureListener() {
+        //     @Override
+        //     // 在 nio 线程连接建立好之后，会调用 operationComplete
+        //     public void operationComplete(ChannelFuture future) throws Exception {
+        //         Channel channel = future.channel();
+        //         log.debug("{}", channel);
+        //         channel.writeAndFlush("hello, world");
+        //     }
+        // });
+    }
+}
---
+++ src/test/java/cn/itcast/netty/c3/EventLoopServer.java
@@ -0,0 +1,56 @@
+package cn.itcast.netty.c3;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.Charset;
+
+/**
+ * @projectName: netty-demo
+ * @package: cn.itcast.netty.c3
+ * @className: EventLoopServer
+ * @author: Gerrad
+ * @description:
+ * @date: 2023/9/30 15:26
+ * @version: 1.0
+ */
+
+@Slf4j
+public class EventLoopServer {
+    public static void main(String[] args) {
+        // 细分2：创建一个独立的 EventLoopGroup
+        EventLoopGroup group = new DefaultEventLoopGroup();
+        new ServerBootstrap()
+                // boss 和 worker
+                // 细分1：boss 只负责 ServerSocketChannel 上 accept 事件     worker 只负责 socketChannel 上的读写
+                .group(new NioEventLoopGroup(), new NioEventLoopGroup(2))
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast("handler1", new ChannelInboundHandlerAdapter() {
+                            @Override                                         // ByteBuf
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf buf = (ByteBuf) msg;
+                                log.debug(buf.toString(Charset.defaultCharset()));
+                                // ctx.fireChannelRead(msg); // 让消息传递给下一个handler
+                            }
+                        });
+                        /*.addLast(group, "handler2", new ChannelInboundHandlerAdapter() {
+                            @Override                                         // ByteBuf
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                ByteBuf buf = (ByteBuf) msg;
+                                log.debug(buf.toString(Charset.defaultCharset()));
+                            }
+                        });*/
+                    }
+                })
+                .bind(8080);
+    }
+}
+
