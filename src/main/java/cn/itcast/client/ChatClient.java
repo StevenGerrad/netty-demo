7f6dc1adafb5e9272ba532fef9bdc6198ac8631d
StevenGerrad <1339807619@qq.com>
2023-10-02 22:38:27 +0800

    P118 netty进阶-聊天业务-心跳

--- src/main/java/cn/itcast/client/ChatClient.java
+++ src/main/java/cn/itcast/client/ChatClient.java
@@ -10,6 +10,9 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
@@ -40,6 +43,22 @@ public class ChatClient {
                     ch.pipeline().addLast(new ProcotolFrameDecoder());
                     // ch.pipeline().addLast(LOGGING_HANDLER);
                     ch.pipeline().addLast(MESSAGE_CODEC);
+                    // 用来判断是不是 读空闲时间过长，或 写空闲时间过长
+                    // 3s 内如果没有向服务器写数据，会触发一个 IdleState#WRITER_IDLE 事件
+                    ch.pipeline().addLast(new IdleStateHandler(0, 3, 0));
+                    // ChannelDuplexHandler 可以同时作为入站和出站处理器
+                    ch.pipeline().addLast(new ChannelDuplexHandler() {
+                        // 用来触发特殊事件
+                        @Override
+                        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception{
+                            IdleStateEvent event = (IdleStateEvent) evt;
+                            // 触发了写空闲事件
+                            if (event.state() == IdleState.WRITER_IDLE) {
+                                // log.debug("3s 没有写数据了，发送一个心跳包");
+                                ctx.writeAndFlush(new PingMessage());
+                            }
+                        }
+                    });
                     ch.pipeline().addLast("client handler", new ChannelInboundHandlerAdapter(){
 
                         // 接受服务器的响应消息
@@ -56,7 +75,6 @@ public class ChatClient {
                                 // 唤醒system in 线程
                                 WAIT_FOR_LOGIN.countDown();
                             }
-
                         }
                         // 在连接建立后触发 active 事件
                         @Override
@@ -142,6 +160,19 @@ public class ChatClient {
                                     }
                                 }
                             }, "system in").start();
+                        }
+                        // 在连接断开时触发
+                        @Override
+                        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+                            log.debug("连接已经断开，按任意键退出..");
+                            // EXIT.set(true);
+                        }
+
+                        // 在出现异常时触发
+                        @Override
+                        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+                            log.debug("连接已经断开，按任意键退出..{}", cause.getMessage());
+                            // EXIT.set(true);
                         }
                     });
                 }
