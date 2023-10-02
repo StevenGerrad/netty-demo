c88d38bedc9c2f7803ccbaf0eaaafaf9bb8fa570
StevenGerrad <1339807619@qq.com>
2023-10-02 17:17:50 +0800

    P104 netty进阶-协议设计与解析-测试

--- src/main/java/cn/itcast/protocol/TestMessageCodec.java
+++ src/main/java/cn/itcast/protocol/TestMessageCodec.java
@@ -15,17 +15,19 @@ public class TestMessageCodec {
     public static void main(String[] args) throws Exception {
         EmbeddedChannel channel = new EmbeddedChannel(
                 new LoggingHandler(),
-                // new LengthFieldBasedFrameDecoder(1024, 12, 4, 0, 0),
+                // 配置帧解码器解决黏包半包问题
+                new LengthFieldBasedFrameDecoder(1024, 12, 4, 0, 0),
                 new MessageCodec()
         );
         // encode
         LoginRequestMessage message = new LoginRequestMessage("zhangsan", "123", "张三");
         channel.writeOutbound(message);
 
-        // // decode
-        // ByteBuf buf = ByteBufAllocator.DEFAULT.buffer();
-        // new MessageCodec().encode(null, message, buf);
-        //
+        // decode
+        ByteBuf buf = ByteBufAllocator.DEFAULT.buffer();
+        new MessageCodec().encode(null, message, buf);
+        // channel.writeInbound(buf);
+
         // ByteBuf s1 = buf.slice(0, 100);
         // ByteBuf s2 = buf.slice(100, buf.readableBytes() - 100);
         // s1.retain(); // 引用计数 2
